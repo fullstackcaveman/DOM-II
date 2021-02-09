1fa4eb1592a4863e6a56b816b15129617dc07d4a
Chris Burton <chris@fullstackcaveman.com>
2021-02-09 12:56:21 -0700

    Added preventDefault to Nav Links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const nav = document.querySelectorAll('nav a');
+const destinations = document.querySelector('.content-pick');
+
+const testLink = document.querySelector('nav a:nth-child(1)');
+
+testLink.addEventListener('click', (e) => {
+	// console.log(e.type);
+});
+
+Array.from(nav).forEach((link) => {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		console.log(e.type);
+	});
+});
