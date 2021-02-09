b6deaa9525d96bd245f3398d482109dd55a4892e
Chris Burton <chris@fullstackcaveman.com>
2021-02-09 13:27:03 -0700

    Set up initial Header resize on scroll functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Your code goes here
-const nav = document.querySelectorAll('nav a');
+const header = document.querySelector('header');
+const navLinks = document.querySelectorAll('nav a');
 const destinations = document.querySelector('.content-pick');
 
 const testLink = document.querySelector('nav a:nth-child(1)');
@@ -8,9 +9,19 @@ testLink.addEventListener('click', (e) => {
 	// console.log(e.type);
 });
 
-Array.from(nav).forEach((link) => {
+Array.from(navLinks).forEach((link) => {
 	link.addEventListener('click', (e) => {
 		e.preventDefault();
 		console.log(e.type);
 	});
 });
+
+// Resize Header on scroll function
+window.addEventListener('scroll', fixHeader);
+function fixHeader() {
+	if (window.scrollY > header.offsetHeight + 50) {
+		header.classList.add('scroll');
+	} else {
+		header.classList.remove('scroll');
+	}
+}
