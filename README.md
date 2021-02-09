150d55a73bf7be848d7d7d8440f1db4196ec0679
Chris Burton <chris@fullstackcaveman.com>
2021-02-09 15:02:26 -0700

    Added shadow to images on mouseover

--- README.md
+++ README.md
@@ -26,13 +26,13 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ### Task 2: Create listeners for 10 types of events
 
 - [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
-  - `mouseover`
+  - `mouseover` [X]
   - `keydown`
   - `wheel`
   - `load`
   - `focus`
   - `resize`
-  - `scroll`
+  - `scroll` [X]
   - `select`
   - `dblclick`
   - `drag / drop`
--- js/index.js
+++ js/index.js
@@ -2,17 +2,33 @@
 const header = document.querySelector('header');
 const navLinks = document.querySelectorAll('nav a');
 const destinations = document.querySelector('.content-pick');
+const images = document.querySelectorAll('img');
 
-const testLink = document.querySelector('nav a:nth-child(1)');
+// const testLink = document.querySelector('nav a:nth-child(1)');
 
-testLink.addEventListener('click', (e) => {
-	// console.log(e.type);
-});
+// testLink.addEventListener('click', (e) => {
+// 	// console.log(e.type);
+// });
 
+// Add preventDefault to Header Links
 Array.from(navLinks).forEach((link) => {
 	link.addEventListener('click', (e) => {
 		e.preventDefault();
 		console.log(e.type);
+	});
+});
+
+// Add mouseover to Header Links
+Array.from(navLinks).forEach((link) => {
+	link.addEventListener('mouseover', () => {
+		link.style.color = '#f34546';
+	});
+});
+
+// Add mouseleave to Header Links
+Array.from(navLinks).forEach((link) => {
+	link.addEventListener('mouseleave', () => {
+		link.style.color = '#212529';
 	});
 });
 
@@ -25,3 +41,18 @@ function fixHeader() {
 		header.classList.remove('scroll');
 	}
 }
+
+// Image manipulation
+// Add mouseover to images
+Array.from(images).forEach((image) => {
+	image.addEventListener('mouseover', () => {
+		image.classList.add('img-shadow');
+	});
+});
+
+// // Add mouseleave to images
+Array.from(images).forEach((image) => {
+	image.addEventListener('mouseleave', () => {
+		image.classList.remove('img-shadow');
+	});
+});
